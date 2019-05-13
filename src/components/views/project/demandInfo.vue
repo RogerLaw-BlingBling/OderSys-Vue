3ce8a398476065d3a63208998bc47096db5c8aad
RogerLaw <893049558@qq.com>
2019-05-13 20:37:22 +0800

    需求增加下载

--- src/components/views/project/demandInfo.vue
+++ src/components/views/project/demandInfo.vue
@@ -1,46 +1,48 @@
 <template>
   <div class="filelist">
     <div class="toolbar">
-      <el-row :gutter="20">
-        <el-col :span="6">
-          <el-select
-            v-model="value"
-            filterable
-            placeholder="请选择"
-            size="small"
-            style="width:330px;min-height:60px"
-          >
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            ></el-option>
-          </el-select>
-        </el-col>
-        <el-col :span="6">
-          <el-upload>
-            <el-button type="primary" size="small">选择文件</el-button>
-            <el-button
-              style="margin-left: 10px;"
+      <el-form :model="fileList">
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <el-select
+              v-model="selectedProject"
+              value-key="id"
+              filterable
+              placeholder="请选择"
               size="small"
-              type="success"
-              @click="submitUploadButton"
-            >上传到服务器</el-button>
-          </el-upload>
-        </el-col>
-      </el-row>
+              style="width:330px;min-height:60px"
+            >
+              <el-option
+                v-for="item in options"
+                :key="item.id"
+                :label="item.projectName"
+                :value="item"
+              ></el-option>
+            </el-select>
+          </el-col>
+          <el-col :span="6">
+            <el-upload action="https://localhost:8080">
+              <el-button type="primary" size="small">选择文件</el-button>
+              <el-button
+                style="margin-left: 10px;"
+                size="small"
+                type="success"
+                @click="submitUploadButton"
+              >上传到服务器</el-button>
+            </el-upload>
+          </el-col>
+        </el-row>
+      </el-form>
     </div>
 
     <template>
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="demandFileName" label="需求文件" width="500"></el-table-column>
-        <el-table-column prop="demandTime" label="客户名称" width="300"></el-table-column>
-        <el-table-column prop="demandTime" label="项目名" width="300"></el-table-column>
-        <el-table-column prop="demandTime" label="时间" width="230"></el-table-column>
+      <el-table :data="demandDataTable" style="width: 100%">
+        <el-table-column prop="demand.title" label="需求文件" width="500"></el-table-column>
+        <el-table-column prop="project.projectName" label="项目名" width="300"></el-table-column>
+        <el-table-column prop="demand.uploadTime" label="时间" width="230"></el-table-column>
         <el-table-column label="文件操作" width="200">
           <template slot-scope="scope">
-            <el-button type="text" size="mini" @click="downLoadButton(scope.$index, scope.row)">下载</el-button>
+            <el-button type="text" size="mini" @click="downLoadButton(scope.row)">下载</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -58,48 +60,53 @@
 </template>
 
 <script>
+import axios from "axios";
+import { connect } from "net";
 export default {
   data() {
     return {
-      //下拉列表
+      demandDataTable: [],
+
+      fileList: {},
+
+      currentPage: 1, //当前页
+      pageSize: 10, //显示条数
+      total: 0, //总数
+
+      //下拉工程名列表
       options: [
-        {
-          value: "选项1",
-          label: "黄金糕"
-        },
-        {
-          value: "选项2",
-          label: "双皮奶"
-        },
-        {
-          value: "选项3",
-          label: "蚵仔煎"
-        },
-        {
-          value: "选项4",
-          label: "龙须面"
-        },
-        {
-          value: "选项5",
-          label: "北京烤鸭"
-        }
+        // {
+        //   value: '选项1',
+        //   projectName: '黄金糕'
+        // }
       ],
       value: "",
+      selectedProject: {}
 
       // fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
-      tableData: [
-        {
-          demandFileName: "广州大学松田学院15计算机科学与技术2班个人毕业设计",
-          demandTime: "2019-05-26"
-        }
-      ]
     };
   },
   methods: {
-    loadData() {},
+    loadData() {
+      axios.get("/project").then(res => {
+        this.options = res.data.content;
+        console.log("方法有效");
+      });
+
+      const page = this.currentPage;
+      const pageSize = this.pageSize;
+      axios.get("/demand").then(res => {
+        this.demandDataTable = res.data.content;
+        this.total = res.data.totalElements;
+      });
+    },
+
     submitUploadButton() {
       // this.$refs.upload.submit();
     },
+
+    downLoadButton(){},
+
     handleRemove(file, fileList) {
       // console.log(file, fileList);
     },
@@ -118,10 +125,10 @@ export default {
       this.currentPage = val; //丢进去查询里，重新查询
       console.log(val);
       this.loadData();
-    },
-    mounted() {
-      this.loadData();
     }
+  },
+  mounted() {
+    this.loadData();
   }
 };
 </script>
